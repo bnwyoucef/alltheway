1497d9c83ce8d69d2041e271e1a471cdb5088f31
Youcef Bounoua <youcef@hotello.com>
2025-03-24 13:36:56 +0100

    fix logo position & rating design

--- components/hero.tsx
+++ components/hero.tsx
@@ -12,17 +12,27 @@ export default function HeroSection() {
 					We handle your bags
 				</h1>
 				<div className="flex items-center mt-4">
-					<span className="text-gray-400 text-sm">4.9/5 Rating</span>
+					<span className="text-gray-400 text-sm"><span className="text-white">4.9/5</span> Rating</span>
 					<div className="flex items-center ml-2">
 						{/* Trustpilot Stars */}
-						<span className="text-green-500">★★★★★</span>
+						{[1, 2, 3, 4].map((_, i) =>
+							<div key={i} className="flex justify-center items-center w-4 h-4 bg-[#219653] mr-0.5
+							">
+								<span className="text-white">★</span>
+							</div>
+						)}
+						<div className="flex justify-center items-center w-4 h-4 bg-[#219653] mr-0.5
+							">
+							<span className="text-white">★</span>
+							<div className="w-1 h-4 bg-white"></div>
+						</div>
 					</div>
-					<span className="ml-2 text-gray-400 text-sm">Trustpilot</span>
+					<span className="ml-2 text-[#BEBEBE] font-semibold text-sm">Trustpilot</span>
 				</div>
 				<button className="mt-6 flex items-center bg-yellow-400 text-black font-bold py-3 px-6 rounded-md hover:bg-yellow-500 transition cursor-pointer">
 					Book now <ArrowRight className="ml-2" />
 				</button>
-				<p className="mt-4 text-gray-400">Starting from $25</p>
+				<p className="mt-4 text-gray-400 font-bold">Starting from $25</p>
 			</div>
 
 			<div className='absolute opacity-20 left-[60px] top-7 w-22 h-22 bg-[#D9AB5233]'></div>
@@ -33,17 +43,19 @@ export default function HeroSection() {
 			<div className='absolute opacity-20 left-[45%] bottom-0 w-22 h-22 bg-[#D9AB5233]'></div>
 
 			{/* bag Image */}
-			<div className='absolute left-[51%] top-[220px] bg-yellow-400 z-20 py-2 pl-2 -rotate-5'>
-				<Image
-					className="cursor-pointer"
-					src="/alltheway_logo.png"
-					alt="alltheway logo"
-					width={110}
-					height={31}
-					priority
-				/>
-			</div>
 			<div className="absolute top-[92px] h-[356px] w-[360px] z-10 left-[43%]">
+				<div className="h-full flex justify-center items-center">
+					<div className='w-[110px] -mt-16 bg-yellow-400 z-20 py-2 pl-2 -rotate-5'>
+						<Image
+							className="cursor-pointer"
+							src="/alltheway_logo.png"
+							alt="alltheway logo"
+							width={110}
+							height={31}
+							priority
+						/>
+					</div>
+				</div>
 				<Image src="/bag.png" fill alt="Suitcase" className="drop-shadow-lg" />
 			</div>
 
